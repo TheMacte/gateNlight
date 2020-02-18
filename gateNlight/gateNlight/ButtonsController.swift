ba4e5badcdc478a05175d6549319c9d5558eab17
Oleg Dmitriev <oleg@acft.pro>
2020-02-18 16:41:24 +0300

    betta is readed.

--- gateNlight/gateNlight/ButtonsController.swift
+++ gateNlight/gateNlight/ButtonsController.swift
@@ -9,26 +9,52 @@
 import UIKit
 
 class ButtonsController: UIViewController {
-
-    @IBOutlet weak var keyTest: UILabel!
-    
     @IBAction func gateButton(_ sender: Any) {
+        UINotificationFeedbackGenerator().notificationOccurred(.success) //Вибрация
         letDoIt(command: commands[Commands.gate] ?? "")
     }
     
     @IBAction func lightButton(_ sender: Any) {
+        UINotificationFeedbackGenerator().notificationOccurred(.success) //Вибрация
         letDoIt(command: commands[Commands.light] ?? "")
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        checkKey(lable: keyTest)
-        
+    //Запрос
+    private func letDoIt(command:String){
+        var urlLight = URL(string: "https://\(UserDefaults.standard.string(forKey: "serverAdderes") ?? "")/\(UserDefaults.standard.string(forKey: "keyForApi") ?? "")/\(command)")
+        guard let requestUrl = urlLight else { fatalError() }
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check if Error took place
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Read HTTP Response Status code
+            if let response = response as? HTTPURLResponse {
+                print("Response HTTP Status code: \(response.statusCode)")
+            }
+            
+            // Convert HTTP Response Data to a simple String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+
+            }
+            
+        }
+        task.resume()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        todo(newKey:newKey,newServer:newServer)
+    private enum Commands {
+        case gate
+        case light
     }
 
+    private var commands:[Commands:String] = [
+        Commands.gate:"worcker_gate.php",
+        Commands.light:"worcker_light.php"
+        ]
 }
